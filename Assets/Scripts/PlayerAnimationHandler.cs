23bf1cd6321db2415af6ee535556e1d16141cc16
Antoniowski <r97.antonio@gmail.com>
2023-01-28 19:07:50 +0100

    Dodge function aggiunta una prima versione del dodge

--- Assets/Scripts/PlayerAnimationHandler.cs
+++ Assets/Scripts/PlayerAnimationHandler.cs
@@ -13,7 +13,6 @@ public class PlayerAnimationHandler : MonoBehaviour
     private Animator animator;
 
     private int isMovingHash;
-    private int isJumpingHash;
     private int isRunningHash;
 
     // Start is called before the first frame update
@@ -26,7 +25,6 @@ public class PlayerAnimationHandler : MonoBehaviour
         //Hashmap is useful to increase performances
 
         isMovingHash = Animator.StringToHash("IsMoving");
-        isJumpingHash = Animator.StringToHash("IsJumping");
         isRunningHash = Animator.StringToHash("IsRunning");
     
     }
@@ -35,16 +33,15 @@ public class PlayerAnimationHandler : MonoBehaviour
     void Update()
     {
         input = playerMovement.GetInput();
-        bool isMoving = animator.GetBool(isMovingHash);
-        bool isJumping = animator.GetBool(isJumpingHash);
-        bool isRunning = animator.GetBool(isRunningHash);
+        bool moving = animator.GetBool(isMovingHash);
+        bool running = animator.GetBool(isRunningHash);
 
         if(input != Vector3.zero){
-            if(!playerMovement.GetStatus().isRunning && isMoving == false){
+            if(!playerMovement.GetStatus().isRunning && moving == false){
                 animator.SetBool("IsMoving", true);
                 animator.SetBool("IsRunning", false);
             }
-            if(playerMovement.GetStatus().isRunning && isRunning == false){
+            if(playerMovement.GetStatus().isRunning && running == false){
                 animator.SetBool("IsRunning", true);
                 animator.SetBool("IsMoving", false);
             }
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -15,8 +15,14 @@ public class PlayerMovement : MonoBehaviour
     private const float FRICTION = 20f;
     private const float ROTATION_SPEED = 15f;
     private const float JUMP_POWER = 10f;
-    private const float GRAVITY = 2f;
-
+    private const float GRAVITY = 1f;
+    
+    //Costanti per il dodge
+    public const float DODGE_VELOCITY = 50f;
+    private float DODGE_DURATION;
+    
+    [SerializeField] AnimationCurve dodgeCurve;
+    
     //VETTORI
     private Vector2 velocity = new Vector2();
     private float verticalVelocity = 0;
@@ -30,16 +36,16 @@ public class PlayerMovement : MonoBehaviour
     public struct PlayerStatus{
         public bool isMoving;
         public bool isRunning;
+        public bool isDodging;
 
         public PlayerStatus(bool initialStatus){
             isMoving = initialStatus;
             isRunning = initialStatus;
+            isDodging = initialStatus;
         }
     };
     private PlayerStatus status;
 
-
-    // Start is called before the first frame update
 
     void Awake(){
 
@@ -47,30 +53,42 @@ public class PlayerMovement : MonoBehaviour
         inputController = new PlayerInputController();
         status = new PlayerStatus(false);
 
+        //movement
         inputController.CharacterInputController.Move.started += OnMovement;
         inputController.CharacterInputController.Move.canceled += OnMovement;
         //performed is needed for controllers sticks
         inputController.CharacterInputController.Move.performed += OnMovement;
+
+        //run
         inputController.CharacterInputController.Run.started += OnRun;
         inputController.CharacterInputController.Run.canceled += OnRun;
 
-
-
+        //dodge
+        //inputController.CharacterInputController.Dodge.IsPressed += OnDodge;
+        //inputController.CharacterInputController.Dodge.canceled += OnDodge;
 
     }
 
+    //funzione di movimento - ottiene ed elabora l'input dal nuovo input controller
     void OnMovement(InputAction.CallbackContext context){
         inputDecoder = context.ReadValue<Vector2>();
 
-        input.x = inputDecoder.x;
-        input.z = inputDecoder.y;
-        input = Quaternion.Euler(new Vector3(0,45,0))*input;
+        if(status.isDodging == false){
+            input.x = inputDecoder.x;
+            input.z = inputDecoder.y;
+            input = Quaternion.Euler(new Vector3(0,45,0))*input;
 
-        status.isMoving = inputDecoder.x != 0 || inputDecoder.y != 0;
+
+            status.isMoving = inputDecoder.x != 0 || inputDecoder.y != 0;
+            }
     }
 
     void OnRun(InputAction.CallbackContext context){
         status.isRunning = context.ReadValueAsButton();
+    }
+
+    void OnDodge(InputAction.CallbackContext context){
+       if(!status.isDodging) status.isDodging = context.ReadValueAsButton();
     }
 
     void OnEnable(){
@@ -81,41 +99,8 @@ public class PlayerMovement : MonoBehaviour
         inputController.CharacterInputController.Disable();
     }
 
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-        if(input != Vector3.zero){
-
-            velocity += status.isRunning ? new Vector2(input.x, input.z)*ACCELERATION*2f*Time.deltaTime : new Vector2(input.x, input.z)*ACCELERATION*Time.deltaTime;
-            velocity = status.isRunning ? Vector2.ClampMagnitude(velocity, MAX_SPEED*1.5f) : Vector2.ClampMagnitude(velocity, MAX_SPEED);
-            HandlePlayerOrientation();
-        }else{
-
-            velocity = Vector2.MoveTowards(velocity, Vector2.zero, FRICTION);
-        }
-
-
-        //NON FUNZIONA
-        if(Input.GetButton("Jump") && controller.isGrounded){
-            //animator.SetBool("IsJumping", true);
-            verticalVelocity = JUMP_POWER;
-        }
-
-        //GRAVITY
-        verticalVelocity -= GRAVITY*Time.deltaTime;
-        controller.Move(new Vector3(velocity.x, verticalVelocity, velocity.y)*Time.deltaTime);
-    }
 
     void HandlePlayerOrientation(){
-        //Faster method
-        //transform.forward = input;
-
         Quaternion toRotation = Quaternion.LookRotation(input, Vector3.up);
         transform.rotation = Quaternion.Slerp(transform.rotation, toRotation, ROTATION_SPEED*Time.deltaTime);
     }
@@ -126,5 +111,49 @@ public class PlayerMovement : MonoBehaviour
 
     public PlayerStatus GetStatus(){
         return status;
+    }
+
+    IEnumerator Dodge(){
+        status.isDodging = true;
+        Animator anim = FindObjectOfType<Animator>();
+        anim.SetTrigger("IsDodging");
+        float timer = 0;
+        while(timer < DODGE_DURATION){
+            float speed = dodgeCurve.Evaluate(timer);
+            velocity = new Vector2(input.x, input.z)*DODGE_VELOCITY*Time.deltaTime;
+            controller.Move(new Vector3(velocity.x, verticalVelocity, velocity.y)*Time.deltaTime);
+            timer += Time.deltaTime;
+            yield return null;
+        }
+        status.isDodging = false;
+    }
+
+       // Start is called before the first frame update
+    void Start()
+    {
+        Keyframe dodgeLastFrame = dodgeCurve[dodgeCurve.length - 1];
+        DODGE_DURATION = dodgeLastFrame.time;
+    }
+
+    // Update is called once per frame
+    void Update()
+   {
+        if(!status.isDodging){
+            if(input != Vector3.zero){
+                velocity += status.isRunning ? new Vector2(input.x, input.z)*ACCELERATION*2f*Time.deltaTime : new Vector2(input.x, input.z)*ACCELERATION*Time.deltaTime;
+                velocity = status.isRunning ? Vector2.ClampMagnitude(velocity, MAX_SPEED*1.5f) : Vector2.ClampMagnitude(velocity, MAX_SPEED);
+                HandlePlayerOrientation();
+            }else{
+                velocity = Vector2.MoveTowards(velocity, Vector2.zero, FRICTION);
+            }
+        }
+
+        if(inputController.CharacterInputController.Dodge.IsPressed()){
+            if(input != Vector3.zero) StartCoroutine(Dodge());
+        }
+
+        //GRAVITA'
+        verticalVelocity -= GRAVITY*Time.deltaTime;
+        controller.Move(new Vector3(velocity.x, verticalVelocity, velocity.y)*Time.deltaTime);
     }
 }
