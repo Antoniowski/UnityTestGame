b83bd799109ad92a092363f8a7217fde179ee245
Antoniowski <r97.antonio@gmail.com>
2023-02-15 13:08:18 +0100

    Aggiunte nuove animazioni di attacco

--- Assets/Scripts/OldScripts/WeaponInfo.cs
+++ Assets/Scripts/OldScripts/WeaponInfo.cs
@@ -8,14 +8,29 @@ public class WeaponInfo : MonoBehaviour
     [SerializeField] private string itemName;
     [SerializeField] private string itemDescription;
     [SerializeField] private int damage;
+
+    public Vector3 interactionCollider;
+    public Vector3 hitBox;
+
     public bool isPlayer;
     private bool pickable = true;
     public LayerMask layerMask;
+
+
+    public string weaponAnimation;
+
+
     // Start is called before the first frame update
     void Start()
     {
         itemName = transform.name;
         itemDescription = "dummy";
+        try
+        {
+            GetComponent<BoxCollider>().size = transform.lossyScale;
+        }catch{
+            print("Il collider non esiste");
+        }
     }
 
     // Update is called once per frame
